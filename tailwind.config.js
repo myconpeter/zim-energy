7a2e208938187dc2c307def18a234736859a9548
myconpeter <79177019+myconpeter@users.noreply.github.com>
2023-08-25 16:10:16 +0100

    added package to front end

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+      colors: {
+        brightYellow: 'hsl(44, 100%, 51%)',
+        black:        'hsl(0, 0%, 3%)',
+        darkGreen:    'hsl(105, 68%, 33%)',
+        lightGreen:   'hsl(119, 92%, 51%)',
+        normalGray:   'hsl(105, 3%, 71%)',
+        lightGray:    'hsl(105, 8%, 89%)',
+        veryDarkGreen:'hsl(105, 94%, 9%)',
+        darkYellow:   'hsl(59, 90%, 32%)',
+        veryLightGrey:'hsl(0, 0%, 98%)',
+      },
+    },
+  },
+  plugins: [],
+}
